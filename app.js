46bc13d29b61c84095719f905283b8a09918b646
vvictorious <vvictorious30@gmail.com>
2019-04-03 15:54:55 -0600

    nav links are successfully navigating to the desired spots without glitches

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const aboutBtn =document.querySelector('.about-btn')
 
 hamburger.addEventListener('click', () => {
 	navLinks.classList.toggle('open');
+	links.forEach(link => {
+		link.classList.toggle('fade');
+	});
+});
+
+aboutBtn.addEventListener('click', () =>{
+	navLinks.classList = 'nav-links';
 	links.forEach(link => {
 		link.classList.toggle('fade');
 	})
