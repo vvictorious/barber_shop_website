d223400fd635f1d14f39a3c3be0219bbd06e3e46
vvictorious <vvictorious30@gmail.com>
2019-04-15 15:27:34 -0600

    done with the contact part

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const about = document.querySelector('.about-btn');
 const services = document.querySelector('.services-btn');
+const contacts = document.querySelector('.contacts-btn');
 const links = document.querySelectorAll('.nav-links li')
 
 hamburger.addEventListener('click', () => {
 	navLinks.classList.toggle('open');
-})
+});
 
 about.addEventListener('click', () => {
 	navLinks.classList.toggle('open')
-})
+});
 
 services.addEventListener('click', () => {
+	navLinks.classList.toggle('open')
+});
+
+contacts.addEventListener('click', () => {
 	navLinks.classList.toggle('open')
 })
