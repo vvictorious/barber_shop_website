7608525e202f12d8eb3bcaf667467d58601a7409
vvictorious <vvictorious30@gmail.com>
2019-04-22 13:48:01 -0600

    had to get a new navbar

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const about = document.querySelector('.about-btn');
-const services = document.querySelector('.services-btn');
-const contacts = document.querySelector('.contacts-btn');
-const links = document.querySelectorAll('.nav-links li')
-const toTheTop = document.querySelector('.back-to-top');
-
-hamburger.addEventListener('click', () => {
-	navLinks.classList.toggle('open');
-});
-
-about.addEventListener('click', () => {
-	navLinks.classList.toggle('open')
-});
-
-services.addEventListener('click', () => {
-	navLinks.classList.toggle('open')
-});
-
-contacts.addEventListener('click', () => {
-	navLinks.classList.toggle('open')
-})
-
+// 
 
 
 
